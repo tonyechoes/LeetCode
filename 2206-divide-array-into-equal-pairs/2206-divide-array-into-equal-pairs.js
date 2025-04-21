3f1dcf9bd99fdff09e6cd5e912db1a03b457fd16
tonyechoes <adrwss2@gmail.com>
2025-04-20 23:56:51 -0700

    Time: 6 ms (39.78%), Space: 57.3 MB (35.96%) - LeetHub

---
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var divideArray = function (nums) {
+  // return new Set(nums).size !== nums.length;
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length; i += 2) {
+    if (nums[i] !== nums[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
+};

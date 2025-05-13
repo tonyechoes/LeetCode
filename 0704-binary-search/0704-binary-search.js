4ae7a09f7ebae5c093ff3290b51fbe7dbc44b3b0
tonyechoes <adrwss2@gmail.com>
2025-05-13 14:15:12 -0700

    Time: 0 ms (100%), Space: 57.8 MB (73.9%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    if (nums.indexOf(target) === -1) {
+        return -1;
+    }
+    
+    return nums.indexOf(target)
+};
